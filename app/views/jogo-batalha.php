9386c7e2c462d847a11bed9bb4cc1da257cce178
Paulo-Henrique-Schiavetti <paulohenriqueschiavetti@hotmail.com>
2018-01-06 17:54:59 -0200

    multiplos personagens em tela e um pouco de responsibilidade

--- app/views/jogo-batalha.php
+++ app/views/jogo-batalha.php
@@ -18,7 +18,15 @@
         <div class="game">
             <div class="tela" style="background: url(/public/img/back1.png);">
                 <div class="caixa pequena faixa1">
-                    <p><?=$inimigo['nome']?></p>
+                    <p class="small"><?=$inimigo['nome']?></p>
+                    <div id="ibarra">
+                        <p class="seta">---------------⇀</p>
+                    </div>
+                    <p class="small"><?=$inimigo['nome']?></p>
+                    <div id="ibarra">
+                        <p class="seta">---------------⇀</p>
+                    </div>
+                    <p class="small"><?=$inimigo['nome']?></p>
                     <div id="ibarra">
                         <p class="seta">---------------⇀</p>
                     </div>
@@ -26,8 +34,22 @@
                 <div class="faixa2 char">
                     <img src="/public/img/inimigos/inimigo_<?=$inimigo['id']?>.png">
                 </div>
+                <div class="faixa2 char r2">
+                    <img src="/public/img/inimigos/inimigo_<?=$inimigo['id']?>.png">
+                </div>
+                <div class="faixa2 char r3">
+                    <img src="/public/img/inimigos/inimigo_<?=$inimigo['id']?>.png">
+                </div>
                 <div class="caixa pequena faixa3">
-                    <p><?=$aliado1['apelido']?></p>
+                    <p class="small"><?=$aliado1['apelido']?></p>
+                    <div id="abarra">
+                        <p class="seta">↼---------------</p>
+                    </div>
+                    <p class="small"><?=$aliado1['apelido']?></p>
+                    <div id="abarra">
+                        <p class="seta">↼---------------</p>
+                    </div>
+                    <p class="small"><?=$aliado1['apelido']?></p>
                     <div id="abarra">
                         <p class="seta">↼---------------</p>
                     </div>
@@ -35,20 +57,26 @@
                 <div class="faixa4 char">
                     <img src="/public/img/aliado<?=$aliado1['id']?>.png">
                 </div>
+                <div class="faixa4 char l2">
+                    <img src="/public/img/aliado<?=$aliado1['id']?>.png">
+                </div>
+                <div class="faixa4 char l3">
+                    <img src="/public/img/aliado<?=$aliado1['id']?>.png">
+                </div>
             </div>
             <div class="menu">
                 <div class="hidden" id="ataques">
-                    <input class="item" type="submit" value="<?=$aliado1['ataque1nome']?>" onclick="ataque(<?=$aliado1['ataque1tipo']?>,<?=$aliado1['ataque1sucesso']?>,<?=$aliado1['ataque1danobase']?>,<?=$aliado1['AF']?>,<?=$aliado1['AM']?>,<?=$aliado1['AN']?>,<?=$aliado1['AD']?>,<?=$inimigo['DF']?>,<?=$inimigo['DM']?>,<?=$inimigo['DN']?>,<?=$inimigo['DD']?>)"/>
-                    <input class="item" type="submit" value="<?=$aliado1['ataque2nome']?>" onclick="ataque(<?=$aliado1['ataque2tipo']?>,<?=$aliado1['ataque2sucesso']?>,<?=$aliado1['ataque2danobase']?>,<?=$aliado1['AF']?>,<?=$aliado1['AM']?>,<?=$aliado1['AN']?>,<?=$aliado1['AD']?>,<?=$inimigo['DF']?>,<?=$inimigo['DM']?>,<?=$inimigo['DN']?>,<?=$inimigo['DD']?>)"/>
-                    <input class="item" type="submit" value="<?=$aliado1['ataque3nome']?>" onclick="ataque(<?=$aliado1['ataque3tipo']?>,<?=$aliado1['ataque3sucesso']?>,<?=$aliado1['ataque3danobase']?>,<?=$aliado1['AF']?>,<?=$aliado1['AM']?>,<?=$aliado1['AN']?>,<?=$aliado1['AD']?>,<?=$inimigo['DF']?>,<?=$inimigo['DM']?>,<?=$inimigo['DN']?>,<?=$inimigo['DD']?>)"/>
-                    <input class="item" type="submit" value="<?=$aliado1['ataque4nome']?>" onclick="ataque(<?=$aliado1['ataque4tipo']?>,<?=$aliado1['ataque4sucesso']?>,<?=$aliado1['ataque4danobase']?>,<?=$aliado1['AF']?>,<?=$aliado1['AM']?>,<?=$aliado1['AN']?>,<?=$aliado1['AD']?>,<?=$inimigo['DF']?>,<?=$inimigo['DM']?>,<?=$inimigo['DN']?>,<?=$inimigo['DD']?>)"/>
+                    <input class="item" id="ataque1" type="submit" value="<?=$aliado1['ataque1nome']?>" onclick="ataque(<?=$aliado1['ataque1tipo']?>,<?=$aliado1['ataque1sucesso']?>,<?=$aliado1['ataque1danobase']?>,<?=$aliado1['AF']?>,<?=$aliado1['AM']?>,<?=$aliado1['AN']?>,<?=$aliado1['AD']?>,<?=$inimigo['DF']?>,<?=$inimigo['DM']?>,<?=$inimigo['DN']?>,<?=$inimigo['DD']?>)"/>
+                    <input class="item" id="ataque2" type="submit" value="<?=$aliado1['ataque2nome']?>" onclick="ataque(<?=$aliado1['ataque2tipo']?>,<?=$aliado1['ataque2sucesso']?>,<?=$aliado1['ataque2danobase']?>,<?=$aliado1['AF']?>,<?=$aliado1['AM']?>,<?=$aliado1['AN']?>,<?=$aliado1['AD']?>,<?=$inimigo['DF']?>,<?=$inimigo['DM']?>,<?=$inimigo['DN']?>,<?=$inimigo['DD']?>)"/>
+                    <input class="item" id="ataque3" type="submit" value="<?=$aliado1['ataque3nome']?>" onclick="ataque(<?=$aliado1['ataque3tipo']?>,<?=$aliado1['ataque3sucesso']?>,<?=$aliado1['ataque3danobase']?>,<?=$aliado1['AF']?>,<?=$aliado1['AM']?>,<?=$aliado1['AN']?>,<?=$aliado1['AD']?>,<?=$inimigo['DF']?>,<?=$inimigo['DM']?>,<?=$inimigo['DN']?>,<?=$inimigo['DD']?>)"/>
+                    <input class="item" id="ataque4" type="submit" value="<?=$aliado1['ataque4nome']?>" onclick="ataque(<?=$aliado1['ataque4tipo']?>,<?=$aliado1['ataque4sucesso']?>,<?=$aliado1['ataque4danobase']?>,<?=$aliado1['AF']?>,<?=$aliado1['AM']?>,<?=$aliado1['AN']?>,<?=$aliado1['AD']?>,<?=$inimigo['DF']?>,<?=$inimigo['DM']?>,<?=$inimigo['DN']?>,<?=$inimigo['DD']?>)"/>
                 </div>
                 <div class="caixa" id="mensagem" >                  
                 </div>
                 <div class="caixa pequena">
                     <div class="item" id="rolagem">rolagem: __</div>
                     <input class="item" type="submit" value="atacar" onclick="atacar();"/>
-                    <input class="item" type="submit" value="inventário"/>
+                    <input class="item" type="submit" value="item"/>
                     <input class="item" type="submit" value="fugir"/>
                 </div>
             </div>
--- public/js/batalha.js
+++ public/js/batalha.js
@@ -2,11 +2,13 @@ var ipv = 0;
 var ipvtotal = 0;
 var apv = 0;
 var apvtotal = 0;
+var x = 0;
+var y = 0;
 
 function start(i1, i2, a1, a2){
     setTimeout(function(){
-        document.getElementById("ibarra").innerHTML = "<div class='barra'><i id='pv' class='pv'></i><i id='notpv'></i></div><i id='pvEscrito'></i>";
-        document.getElementById("abarra").innerHTML = "<div class='barra'><i id='aliadopv' class='pv'></i><i id='aliadonotpv'></i></div><i id='aliadopvEscrito'></i>";
+        document.getElementById("ibarra").innerHTML = "<div class='barra'><i id='pv' class='pv'></i><i class='notpv' id='notpv'></i></div><i id='pvEscrito'></i>";
+        document.getElementById("abarra").innerHTML = "<div class='barra'><i id='aliadopv' class='pv'></i><i class='notpv' id='aliadonotpv'></i></div><i id='aliadopvEscrito'></i>";
 
         ipv = i1;    
         ipvtotal = i2;      
@@ -22,8 +24,103 @@ function start(i1, i2, a1, a2){
 function rolar(){
     return(Math.trunc(Math.random()*20+1));
 }
-function ataque(tipo,sucesso,danobase,af,am,an,ad,df,dm,dn,dd){
+function mostrarrolagem(rolagem){
 
+    if(rolagem>=20){
+        document.getElementById("rolagem").innerHTML = "rolagem: <strong class='green'>"+rolagem+"</strong>";
+    } else if(rolagem>=10){
+        document.getElementById("rolagem").innerHTML = "rolagem: <strong>"+rolagem+"</strong>";
+    } else {
+        document.getElementById("rolagem").innerHTML = "rolagem: <strong class='red'>0"+rolagem+"</strong>";
+    }    
+}
+function atacar(){
+    document.getElementById("mensagem").className = "hidden";
+    document.getElementById("ataques").className = "caixa";
+    document.addEventListener("keydown", mover, false);
+    document.addEventListener("keydown", position, false);
+    document.addEventListener("keydown", enter, false);
+    position();
+}
+function mover(key) {
+    if(key.keyCode == 87){
+        x += 1;
+    }
+    if(key.keyCode == 65){
+        y -= 1;
+    }
+    if(key.keyCode == 83){
+        x -= 1;
+    }
+    if(key.keyCode == 68){
+        y += 1;
+    }
+    if(x>1){
+        x = 0;
+    }
+    if(x<0){
+        x = 1;
+    }
+    if(y>1){
+        y = 0;
+    }
+    if(y<0){
+        y = 1;
+    }
+}
+function position() {
+    if (x == 0 && y == 0) {
+        document.getElementById('ataque1').style.backgroundColor = "#202020";
+        document.getElementById('ataque2').style.background = "none";
+        document.getElementById('ataque3').style.background = "none";
+        document.getElementById('ataque4').style.background = "none";
+    } else if (x == 0 && y == 1) {
+        document.getElementById('ataque2').style.backgroundColor = "#202020";
+        document.getElementById('ataque1').style.background = "none";
+        document.getElementById('ataque3').style.background = "none";
+        document.getElementById('ataque4').style.background = "none";
+    } else if (x == 1 && y == 0) {
+        document.getElementById('ataque3').style.backgroundColor = "#202020";
+        document.getElementById('ataque2').style.background = "none";
+        document.getElementById('ataque1').style.background = "none";
+        document.getElementById('ataque4').style.background = "none";
+    } else if (x == 1 && y == 1) {
+        document.getElementById('ataque4').style.backgroundColor = "#202020";
+        document.getElementById('ataque2').style.background = "none";
+        document.getElementById('ataque3').style.background = "none";
+        document.getElementById('ataque1').style.background = "none";
+    }
+}
+function enter(key){
+    if (key.keyCode == 13){
+        if (x == 0 && y == 0) {
+            document.getElementById('ataque1').onclick();    
+            remove();
+        } else if (x == 0 && y == 1) {
+            document.getElementById('ataque1').onclick();  
+            remove();  
+        } else if (x == 1 && y == 0) {
+            document.getElementById('ataque1').onclick();  
+            remove();  
+        } else if (x == 1 && y == 1) {
+            document.getElementById('ataque1').onclick(); 
+            remove();   
+        }
+    }
+}
+function remove() {
+    document.removeEventListener("keydown", mover, false);
+    document.removeEventListener("keydown", position, false);
+}
+function mensagem(mensagem){
+    document.getElementById("ataques").className = "hidden";
+    document.getElementById("mensagem").className = "caixa";
+    document.getElementById("mensagem").innerHTML = mensagem;
+    setTimeout(function() {
+        document.getElementById("mensagem").innerHTML = "<p>O que você vai fazer?</p>";
+    },1400);
+}
+function ataque(tipo,sucesso,danobase,af,am,an,ad,df,dm,dn,dd){
     if (tipo<=4){
         ataquefisico(sucesso,danobase,af,df);
     } else if (tipo<=6){
@@ -163,25 +260,3 @@ function calcularvidaaliado(barra){
     document.getElementById("aliadopvEscrito").innerHTML = apv+"/"+apvtotal;
 }
 
-function atacar(){
-    document.getElementById("mensagem").className = "hidden";
-    document.getElementById("ataques").className = "caixa";
-}
-function mensagem(mensagem){
-    document.getElementById("ataques").className = "hidden";
-    document.getElementById("mensagem").className = "caixa";
-    document.getElementById("mensagem").innerHTML = mensagem;
-    setTimeout(function() {
-        document.getElementById("mensagem").innerHTML = "<p>O que você vai fazer?</p>";
-    },1400);
-}
-function mostrarrolagem(rolagem){
-
-    if(rolagem>=20){
-        document.getElementById("rolagem").innerHTML = "rolagem: <strong class='green'>"+rolagem+"</strong>";
-    } else if(rolagem>=10){
-        document.getElementById("rolagem").innerHTML = "rolagem: <strong>"+rolagem+"</strong>";
-    } else {
-        document.getElementById("rolagem").innerHTML = "rolagem: <strong class='red'>0"+rolagem+"</strong>";
-    }    
-}
