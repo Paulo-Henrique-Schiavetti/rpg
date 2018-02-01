c33bff0281faed549b1b3256e92eac75fc9afb76
Paulo-Henrique-Schiavetti <paulohenriqueschiavetti@hotmail.com>
2018-01-31 22:45:38 -0200

    teletransporte entre um evento e outro, ida e volta, o teletransporte não é ativado novamente se você chegou naquele evento através de outro teletransporte

--- public/js/eventos.js
+++ public/js/eventos.js
@@ -1,24 +1,38 @@
 obj1x = 40;
 obj1y = 60;
+obj1ativo = false;
 obj2x = 60;
 obj2y = 45;
+obj2ativo = false;
 obj3x = 40;
 obj3y = 30;
 obj3ativo = false;
 
 var obj1 = setInterval(function(){
     if (parado){
-        if (x == obj1x && y == obj1y) {
-            teletransporte(x,45);
-        }
+        if(obj1ativo==false){
+            if (x == obj1x && y == obj1y) {
+                teletransporte(60,45);
+                obj2ativo = true; 
+            }
+        } else if (x != obj1x || y != obj1y) {
+            obj1ativo = false;
+            obj2ativo = false;          
+        }  
     }    
 }, 1);
 
 var obj2 = setInterval(function(){
     if (parado){
-        if (x == obj2x && y == obj2y) {
-            teletransporte(40,y);
-        }
+        if(obj2ativo==false){
+            if (x == obj2x && y == obj2y) {
+                teletransporte(40,60);
+                obj1ativo = true; 
+            }
+        } else if (x != obj2x || y != obj2y) {
+            obj1ativo = false;
+            obj2ativo = false;          
+        }  
     }    
 }, 1);
 
